3437d4104c35ae6a9b75ed6dbf055795ecf15bb3
¨csi <paradisemalcolm@gmail.com>
2023-09-18 14:41:03 +0200

    new code for tables

--- app/inclusion/Tableaux/index.php
+++ app/inclusion/Tableaux/index.php
@@ -1,86 +1,101 @@
 <?php
-
-echo "Tableaux";
-#https://www.php.net/manual/en/book.array.php
-
-/*
-//Creates a blank array.
-$theVariable = array();
-
-//Creates an array with elements.
-$theVariable = array("A", "B", "C");
-
-//Creating Associaive array.
-$theVariable = array(1 => "http//google.com", 2=> "http://yahoo.com");
-
-//Creating Associaive array with named keys
-$theVariable = array("google" => "http//google.com", "yahoo"=> "http://yahoo.com");
-
-Note:
-New value can be added to the array as shown below.
-$theVariable[] = "D";
-$theVariable[] = "E";
-*/
-$tableau = ['Groupe Scolaire',34,5.78,false,[1,2,3]];
-// echo $tableau; <-fausse syntaxe!
-echo "<pre>";
-// print_r($tableau); ou:
-var_dump($tableau);
-echo "</pre>";
+echo "index de 'tableaux'";
 echo '<br/><br/><br/>';
 
-$table = [];
-$table = 'January';
-$table = 45;
-$table = false;
+// tableaux indéxés
 
-// this is a variable that is called different times to assign its values 
-echo "</pre>";
+$tablo01 = [];
+// $tablo01 = array (); <- declaration dessuète
 
-var_dump($table);
+// $tablo02 = [true, 4, 5.8, [5, 'client', ['user', 8.9, 8]]];  écrite espacée ci-dessous équivalente
+$tablo02 = [
+    true,
+    4,
+    5.8,
+    [
+        5,
+        'client',
+        [
+            'user',
+            8.9,
+            8
+        ]
+    ]
+];
+echo "<pre>";
+var_dump($tablo02);
+echo '<br/><br/><br/>';
+//--------------------------------------------------------------------------------
+
+$tablo03 = [];
+$tablo03 = 'janvier';
+$tablo03 = 45;
+$tablo03 = false;
+echo "<pre>";
+var_dump($tablo03);
 echo '<br/>';
+// ceci ne définit qu'une seule cellule, qu'on réaffecte à chaque fois...
 
-$table1 = [];
-array_push($table1, 'January', 45, false);
-echo "</pre>";
-var_dump($table1);
+$tablo03 = []; //...ceci marche
+$tablo03[] = 'janvier';
+$tablo03[] = 45;
+$tablo03[] = false;
+echo "<pre>";
+var_dump($tablo03);
+echo '<br/>'; // ou bien ...
+
+// ...pour remplir les cellules les unes à la suite des autres :
+$tablo04 = [];
+array_push($tablo04, 'janvier', 45, false);
+echo "<pre>";
+var_dump($tablo04);
 echo '<br/>';
 
 $notes = [12, 14, 8, 10];
-$tablefusion = array_merge($table1, $notes);
-echo "</pre>";
-var_dump($tablefusion);
+
+$tabloFusion = array_merge($tablo04, $notes);
+echo "<pre>";
+var_dump($tabloFusion);
 echo '<br/>';
 
-// indexes to access the elements of an array
+// les index : accéder aux élements du tableau -----------------------------------
 
-var_dump($notes [1]);
-var_dump($tablefusion [4]);
-print_r($tablefusion [4]);echo '<br/>';
-print_r($tablefusion [0]);echo '<br/>';
-//print_r($tablefusion [3][1]);echo '<br/>'; displays 'client' of the 2nd table
-//print_r($tablefusion [3][2][2]);echo '<br/>'; displays '8' of the 2nd table
+var_dump($notes[1]); // afficher la 2ème cellule du tableau 'notes'
+var_dump($tabloFusion[4]);
+print_r($tabloFusion[4]); echo '<br/>';
+print_r($tabloFusion[0]); echo '<br/>';
+print_r($tablo02[3][1]); echo '<br/>'; //afficher 'client' du tableau 2
+print_r($tablo02[3][2][2]); //afficher '8' du tableau 2
+echo '<br/><br/><br/>';
+
+// tableau associatif ------------------------------------------------------------
+$fruits = [
+    'banane' => 'jaune',
+    'pomme' => 'rouge',
+    'kiwi' => 'vert'    
+];
+
+$fruits['cassis'] = 'violet';
+$fruits['framboise'] = 'rose';
+print_r($fruits);
 echo '<br/>';
 
-//--------------------------------------------------------------------------------------------------------------
+print_r($fruits['kiwi']); // afficher la couleur du kiwi (notion de 'clé' et 'valeur')
+echo '<br/>';
 
-$array1 = [
-    0 => 'apple',
-    1 => 'orange',
-    2 => 'pear',
-];
+$chaine = implode('¤', $fruits); //afficher les VALEURS de chaque cellule dans une seule chaine de charactère avec le séparateur choisit '¤'
+print_r($chaine);
+echo '<br/>';
+// à l'inverse 'explode' prend les mots d'une chaine de charactère et les places en valeurs d'un nouveau tableau
+$fruits2 = explode('¤', $chaine);
+print_r($fruits2);
+echo '<br/><br/><br/>';
 
-$array2 = [
-    0 => 'melon',
-    1 => 'orange',
-    2 => 'banana',
-];
-
-$result = array_keys(
-    array_flip($array1) + array_flip($array2)
-);
+$planets = ['mars', 'terre', 'uranus', 'venus', 'jupiter'];
+print_r($planets); echo '<br/>';
+sort($planets);
+print_r($planets); echo '<br/>';
 
 
 
-
-
+?>
