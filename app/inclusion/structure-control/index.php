cb1f46de8dfa120bf83e2439aac00c354862e408
¨csi <paradisemalcolm@gmail.com>
2023-09-18 10:14:27 +0200

    if in structure controls

--- app/inclusion/structure-control/index.php
+++ app/inclusion/structure-control/index.php
@@ -1,3 +1,13 @@
 <?php
 echo "test";
+
+#https://www.php.net/manual/en/language.control-structures.php
+
+#the usage of if and if else
+$age = 19;
+if($age > 18) {
+    echo "You have the right to vote"; # change the variable age and refresh the page to see the different results
+} else {
+    echo "you don't have the right to vote";
+}
 ?>
