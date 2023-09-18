84a8ea55465852177a1ce05a692438b9351a6a63
¨csi <paradisemalcolm@gmail.com>
2023-09-18 10:20:25 +0200

    new input on the if conditions

--- app/inclusion/structure-control/index.php
+++ app/inclusion/structure-control/index.php
@@ -1,6 +1,8 @@
 <?php
 echo "test";
 
+echo '<br/>'; #to add more lines you can use echo '<br/>''<br/>''<br/>';
+
 #https://www.php.net/manual/en/language.control-structures.php
 
 #the usage of if and if else
@@ -9,5 +11,16 @@ if($age > 18) {
     echo "You have the right to vote"; # change the variable age and refresh the page to see the different results
 } else {
     echo "you don't have the right to vote";
+}
+
+echo '<br/>';
+
+#example 2
+
+$time = 23;
+if($time < 12) {
+    echo "it is in the morning";
+} else {
+    echo "it is in the evening";
 }
 ?>
