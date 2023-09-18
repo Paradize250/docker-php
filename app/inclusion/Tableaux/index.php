a40443a7ff5c550a7085b1cc70108fd79948d40c
¨csi <paradisemalcolm@gmail.com>
2023-09-18 12:27:51 +0200

    new changes

--- app/inclusion/Tableaux/index.php
+++ app/inclusion/Tableaux/index.php
@@ -1,3 +1,24 @@
 <?php
 
 echo "Tableaux";
+#https://www.php.net/manual/en/book.array.php
+
+/*
+//Creates a blank array.
+$theVariable = array();
+
+//Creates an array with elements.
+$theVariable = array("A", "B", "C");
+
+//Creating Associaive array.
+$theVariable = array(1 => "http//google.com", 2=> "http://yahoo.com");
+
+//Creating Associaive array with named keys
+$theVariable = array("google" => "http//google.com", "yahoo"=> "http://yahoo.com");
+
+Note:
+New value can be added to the array as shown below.
+$theVariable[] = "D";
+$theVariable[] = "E";
+*/
+$a = [1, 2, 3, 4];
