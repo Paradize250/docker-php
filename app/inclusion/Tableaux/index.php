c7c9c68238ab8e232b2c9dbeac2110415c9b763d
¨csi <paradisemalcolm@gmail.com>
2023-09-18 14:21:21 +0200

    include elements in the table

--- app/inclusion/Tableaux/index.php
+++ app/inclusion/Tableaux/index.php
@@ -21,4 +21,66 @@ New value can be added to the array as shown below.
 $theVariable[] = "D";
 $theVariable[] = "E";
 */
-$a = [1, 2, 3, 4];
+$tableau = ['Groupe Scolaire',34,5.78,false,[1,2,3]];
+// echo $tableau; <-fausse syntaxe!
+echo "<pre>";
+// print_r($tableau); ou:
+var_dump($tableau);
+echo "</pre>";
+echo '<br/><br/><br/>';
+
+$table = [];
+$table = 'January';
+$table = 45;
+$table = false;
+
+// this is a variable that is called different times to assign its values 
+echo "</pre>";
+
+var_dump($table);
+echo '<br/>';
+
+$table1 = [];
+array_push($table1, 'January', 45, false);
+echo "</pre>";
+var_dump($table1);
+echo '<br/>';
+
+$notes = [12, 14, 8, 10];
+$tablefusion = array_merge($table1, $notes);
+echo "</pre>";
+var_dump($tablefusion);
+echo '<br/>';
+
+// indexes to access the elements of an array
+
+var_dump($notes [1]);
+var_dump($tablefusion [4]);
+print_r($tablefusion [4]);echo '<br/>';
+print_r($tablefusion [0]);echo '<br/>';
+//print_r($tablefusion [3][1]);echo '<br/>'; displays 'client' of the 2nd table
+//print_r($tablefusion [3][2][2]);echo '<br/>'; displays '8' of the 2nd table
+echo '<br/>';
+
+//--------------------------------------------------------------------------------------------------------------
+
+$array1 = [
+    0 => 'apple',
+    1 => 'orange',
+    2 => 'pear',
+];
+
+$array2 = [
+    0 => 'melon',
+    1 => 'orange',
+    2 => 'banana',
+];
+
+$result = array_keys(
+    array_flip($array1) + array_flip($array2)
+);
+
+
+
+
+
