12bab772d7e78dabdc57d50c85cb59f1536a63b4
¨csi <paradisemalcolm@gmail.com>
2023-09-18 12:11:35 +0200

    create more pages from the link

--- app/inclusion/partials/menu.php
+++ app/inclusion/partials/menu.php
@@ -2,7 +2,7 @@
     <li><a href="">Home</a></li>
     <li><a href="./structure-control/index.php">Structures de contrôles</a></li> 
     <!-- to include a new page and create a link make sure that the listed item has the correct path-->
-    <li><a href="">Boucles</a></li>
-    <li><a href="">Tableaux</a></li>
+    <li><a href="./Boucles/index.php">Boucles</a></li>
+    <li><a href="./Tableaux/index.php">Tableaux</a></li>
     <li><a href="">Fonctions</a></li>
 </ul>
--- app/inclusion/structure-control/index.php
+++ app/inclusion/structure-control/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "test";
+echo "Test";
 
 echo '<br/>'; #to add more lines you can use echo '<br/>''<br/>''<br/>';
 
@@ -17,13 +17,117 @@ echo '<br/>';
 
 #example 2 (use of multiple conditions)
 
-$time = 23;
+#$time = 14; this was a used variable example used to see different results
+
+$time = date("H");
 if($time < 12) {
-    echo "Tt is in the morning";
+    echo "It is in the morning";
 } elseif ($time >= 18) {
-    echo "Ut is in the evening";
+    echo "It is in the evening";
 } else {
-    echo "It is the afternoon";
+    echo "It is in the afternoon";
 }
 echo '<br/>';
+
+$a = '20';
+$b = '5';
+
+if($b != 3) {
+    echo "The variable is not equal to 3";
+}
+else {
+    echo "The variable is equal to 3";
+
+}
+echo '<br/>';
+if($b === 5) {
+    echo "The variable is equal in type and value";
+}
+else {
+    echo "The variable is not equal in type and value";
+
+}
+echo '<br/>';
+if($b == 5) {
+    echo "The variable is equal in type";
+}
+    #find the anti slash sign on mac
+
+/* Example
+$a = 5;
+$b = 10;
+
+if ($a != $b) {
+    echo "$a is not equal to $b"; // This will be printed because 5 is not equal to 10
+} else {
+    echo "$a is equal to $b";
+}*/
+
+echo '<br/>';
+
+$true =true; #you can change the variable type between true and false
+if ($true) {
+    echo "True";
+} else {
+    echo "False";
+    #echo(True) ? "true" : "false";
+}
+
+#this can also be written with the python method
+
+/* 
+if (true): 
+    echo "true";
+else 
+    echo "false";
+endif;
+*/
+
+echo '<br/>';
+$votes=7540;
+$votesPrecedents=1254;
+($votes > $votesPrecedents) ? $votes-- : $votes++;
+# the -- and ++ increase or decrease a unit depending on what argument is true. In this case the result is 7539
+echo $votes;
+echo '<br/>';
+
+#switch case
+$color = 'pink';
+switch ($color): #{
+    case 'red':
+        echo "Your favorite color is red";
+        break;
+    case 'blue';
+        echo "Your favorite color is blue";
+        break;
+    case 'green';
+    echo "Your favorite color is green";
+    break;
+    default:
+    echo "Your favorite color is not red, blue, or green";
+endswitch; #}
+
+#match
+
+$food = 'cake';
+
+$return_value = match ($food) {
+    'apple' => 'This food is an apple',
+    'bar' => 'This food is a bar',
+    'cake' => 'This food is a cake',
+    default => 'The food is not available',
+};
+
+var_dump($return_value); #used as an output for variables and tables
+
+
+
+
+
+
+
+
+
+
+
 ?>
