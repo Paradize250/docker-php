837e31f91c96ad23c1e13e316341d460d9e8f497
¨csi <paradisemalcolm@gmail.com>
2023-09-18 10:04:20 +0200

    add a comment for the corrections made

--- app/inclusion/partials/menu.php
+++ app/inclusion/partials/menu.php
@@ -1,6 +1,7 @@
 <ul>
     <li><a href="">Home</a></li>
-    <li><a href="../structure-control">Structures de contrôles</a></li>
+    <li><a href="./structure-control/index.php">Structures de contrôles</a></li> 
+    <!-- to include a new page and create a link make sure that the listed item has the correct path-->
     <li><a href="">Boucles</a></li>
     <li><a href="">Tableaux</a></li>
     <li><a href="">Fonctions</a></li>
